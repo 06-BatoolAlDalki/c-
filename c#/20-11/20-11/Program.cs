d1d27ba3f090bc3f170b06b052c8370596bb7fec
Batool Al-Dalki <115452476+06-BatoolAlDalki@users.noreply.github.com>
2022-11-21 16:08:28 +0300

    Add files via upload
    21-11-tasks

---
+++ c#/20-11/20-11/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _20_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // question 1
+            Console.WriteLine("please enter your favourite string");
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+            Console.WriteLine("/////////////");
+            //question 2
+            double num = 12000;
+            string car = "mercides";
+            char c = 'A';
+            bool batool = true;
+            int num1 = 12;
+            const int num2 = 13;
+
+            Console.WriteLine(num);
+            Console.WriteLine(car);
+            Console.WriteLine(c);
+            Console.WriteLine(batool);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            // Console.WriteLine(" ",car," ",c," ",batool," ",num1," ",num);
+            Console.WriteLine("/////////////");
+            //question3
+            string[] cars = { "mercides", "lonovo", "kashef" };
+
+            Console.WriteLine(cars[0]);
+            Console.WriteLine(cars[1]);
+            Console.WriteLine(cars[2]);
+            Console.WriteLine(cars.Length);
+            Console.WriteLine("/////////////");
+            //question 4
+            Console.WriteLine("your first name");
+            string first = Console.ReadLine();
+            Console.WriteLine("your last name");
+            string Lfirst = Console.ReadLine();
+            Console.WriteLine("your input your year of birth :");
+            string birth = Console.ReadLine();
+            Console.WriteLine(first + " " + Lfirst + " " + birth);
+            Console.WriteLine("/////////////");
+            //question 5
+
+            int[] array_sort = { 2, 1, 4, 6, 5, 8 };
+            Array.Sort(array_sort);
+
+            Console.WriteLine(array_sort[0]);
+            Console.WriteLine(array_sort[1]);
+            Console.WriteLine(array_sort[2]);
+            Console.WriteLine(array_sort[3]);
+            Console.WriteLine(array_sort[4]);
+            Console.WriteLine(array_sort[5]);
+            Console.WriteLine("/////////////");
+            //question 5
+
+            int[] sum = { 12, 10, 20 };
+            Console.WriteLine("Sum of all elements stored in the array is : ");
+           Console.WriteLine(sum[0] + sum[1] + sum[2]);
+           
+
+
+
+        }
+    }
+}
---
+++ c#/21-11/21-11/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//enum
+
+
+public enum Season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
+
+
+// defining struct
+struct Employee
+{
+    public int id;
+
+    public void getId(int id)
+    {
+        Console.WriteLine("Employee Id: " + id);
+    }
+}
+
+
+//class
+//class Car
+//{
+//   public string color = "red";
+//}
+namespace _21_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //data types
+            int x = 10;
+            double y = 20.12;
+            string name = "ayah";
+            bool z = true;
+            char q = 'c';
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(name);
+            Console.WriteLine(z);
+            Console.WriteLine(q);
+
+            //nullable data type
+            Nullable<int> i = null;
+
+            if (i.HasValue)
+                Console.WriteLine(i.Value); // or Console.WriteLine(i)
+            else
+                Console.WriteLine("Null");
+
+            //emnum
+           
+
+            var b = (Season)1;
+            Console.WriteLine(b);  // output: Summer
+
+            var c = (Season)4;
+            Console.WriteLine(c);  // output: 4
+
+            //struct data type
+
+
+            // declare emp of struct Employee
+            Employee emp;
+
+            // accesses and sets struct field
+            emp.id = 100;
+
+            // accesses struct methods
+            emp.getId(emp.id);
+
+            Console.ReadLine();
+
+
+            //////////class
+            // Car myObj = new Car();
+            //Console.WriteLine(myObj.color);
+
+
+            //object 
+            String[] test = { "1", "2" };
+            object o = test;
+
+            if (o is string[])
+            {
+                Console.WriteLine("this is string array");
+            }
+
+
+
+
+
+            ///ahmad
+            int myint = 10;
+            double mydu = myint;
+            Console.WriteLine(mydu);
+            Console.ReadLine();
+
+            double myv = 10.8;
+            int hell = (int)myint;
+            Console.WriteLine(hell);
+
+
+
+
+            //rahma
+            Console.WriteLine("**");
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age : " + age);
+
+
+            Console.WriteLine("***");
+            Console.WriteLine("enter your grade");
+            double grade = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("your grade : " + grade);
+            Console.WriteLine("***");
+            //mayyas
+
+
+            int number = 10;
+            double doubleNumber = 5.25;
+            bool myBool = true;
+            string mayyas = "mayyas";
+
+            Console.WriteLine(Convert.ToString(number));    // Convert int to string
+            Console.WriteLine(Convert.ToDouble(number));    // Convert int to double
+            Console.WriteLine(Convert.ToInt32(doubleNumber));  // Convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // Convert bool to string
+            Console.WriteLine(Convert.ToInt64(doubleNumber));   // Convert bool to string
+            Console.WriteLine(Convert.ToBoolean(doubleNumber));   // Convert double to bool
+            Console.WriteLine(Convert.ToChar(mayyas[2]));   // Convert string to cha
+        }
+
+       
+
+    }
+    }
+
