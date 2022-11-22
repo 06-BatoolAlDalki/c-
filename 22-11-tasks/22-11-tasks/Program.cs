5cfb8e380c8eed86b9f4d09e02ba188a4e2234a3
Batool Al-Dalki <115452476+06-BatoolAlDalki@users.noreply.github.com>
2022-11-22 15:09:13 +0300

    Add files via upload
    update 22-11-tasks

--- 22-11-tasks/22-11-tasks/Program.cs
+++ 22-11-tasks/22-11-tasks/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -93,28 +94,32 @@ namespace _22_11_tasks
 
             Console.WriteLine("enter 3 inputs");
             string str=Console.ReadLine();
-            string s1 = "";
-            string s2 = "";
-            string s3 = "";
-           for(int i=0;i<str.Length;i++)
-            {
-                s1=str.Substring(0,1);
-                s2=str.Substring(2,1);
-                s3=str.Substring(4,1);
-               
+            //  string s1 = "";
+            //  string s2 = "";
+            //  string s3 = "";
+            // for(int i=0;i<str.Length;i++)
+            //  {
+            //      s1=str.Substring(0,1);
+            //      s2=str.Substring(2,1);
+            //      s3=str.Substring(4,1);
+
+            //  }
+            //int n1=Convert.ToInt32(s1);
+            //  int n2 = Convert.ToInt32(s2);
+            //  int n3 = Convert.ToInt32(s3);
+            //  Console.WriteLine(n1+n2+n3);
+
+            string[] s = str.Split(',');
+            int sum1 = 0;
+            foreach (string s2 in s) {
+                sum1=sum1+ Convert.ToInt32( s2);
             }
-          int n1=Convert.ToInt32(s1);
-            int n2 = Convert.ToInt32(s2);
-            int n3 = Convert.ToInt32(s3);
-            Console.WriteLine(n1+n2+n3);
-
-
-
+            Console.WriteLine(sum1);
 
             Console.WriteLine("\n");
             Console.WriteLine("///////task5");
             int sum = 0;
-            for(int i=1;i<100;i+=1)
+            for(int i=1;i<100;i+=2)
             {
                 sum += i; 
             }
@@ -145,8 +150,20 @@ namespace _22_11_tasks
             }
 
 
-
-
+            Console.WriteLine("////////////////task8");
+            int[] array =new int[] {1,2,3,4,5,3,2,1 };
+            int[] distinct = array.Distinct().ToArray();
+            int s4 = 0;
+            //Console.WriteLine(String.Join(",", distinct));
+            for(int i=0;i<distinct.Length;i++)
+            {
+                s4+= array[i];
+            
+            }
+           
+            Console.WriteLine(s4);
+            Console.WriteLine(array.Length);
+            Console.WriteLine( s4/distinct.Length);
         }
     }
 }
