491f73da20921e2956608c9d0d8ce1565a199c0d
Batool Al-Dalki <115452476+06-BatoolAlDalki@users.noreply.github.com>
2022-11-29 20:08:40 +0300

    29-11

--- task-29-11/task-29-11/Program.cs
+++ task-29-11/task-29-11/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,10 @@ namespace task_29_11
         public string Name { get; set; }
         public string Birh_date { get; set; }
 
+        public void printt()
+        {
+            Console.WriteLine(Id+ " "+Name+" "+Birh_date);
+        }
    public     Employee(int a, string b, string c) {
             Id = a;
             Name = b;
@@ -21,17 +26,24 @@ namespace task_29_11
         public void Calc ()
             {
             //Console.WriteLine();
-            var birthday = new DateTime(1999, 1, 7);
-            int age = (int)((DateTime.Now - birthday).TotalDays / 365.242199);
-            Console.WriteLine(age);
+            //var birthday = new DateTime(1999, 1, 7);
+            //int age = (int)((DateTime.Now - birthday).TotalDays / 365.242199);
+            //Console.WriteLine(age);
+            DateTime dob = Convert.ToDateTime(Birh_date);
+            DateTime PresentYear = DateTime.Now;
+            TimeSpan ts = PresentYear - dob;
+            DateTime Age = DateTime.MinValue.AddDays(ts.Days);
+            Console.WriteLine(string.Format(" {0} Years {1} Month {2} Days", Age.Year - 1, Age.Month - 1, Age.Day - 1));
         }
 
-        public void Calc(int a, int b, int c)
+        public void Calc(string s)
         {
-            //Console.WriteLine();
-            var birthday = new DateTime(a, b, c);
-            int age = (int)((DateTime.Now - birthday).TotalDays / 365.242199);
-            Console.WriteLine(age);
+            Birh_date = s;
+            DateTime dob = Convert.ToDateTime(Birh_date);
+            DateTime PresentYear = DateTime.Now;
+            TimeSpan ts = PresentYear - dob;
+            DateTime Age = DateTime.MinValue.AddDays(ts.Days);
+            Console.WriteLine(string.Format(" {0} Years {1} Month {2} Days", Age.Year - 1, Age.Month - 1, Age.Day - 1));
         }
         public virtual void print() {
             Console.WriteLine("the Id of Employee" );
@@ -49,23 +61,28 @@ namespace task_29_11
 
         public override void print()
         {
-            Console.WriteLine("the Id of Manger");
+            Console.WriteLine("Hello Manger");
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Employee e = new Employee(1, "batool", "7-1-1999");
-            e.Calc();
+           // Employee e1 = new Manger(1,"batool", "18 Feb 1987");
+           Employee e = new Manger(1, "batool", "18 Feb 1987");
+           
+            
             Manger manger = new Manger(1,"batool","1999");
-            Console.WriteLine( "the ID of employee"+manger.Id);
-            Console.WriteLine("the name of employee" + manger.Name);
-            Console.WriteLine("the age of employee");
-            manger.Calc();
-            manger.Calc(2000, 1, 7);
-            manger.print();
-
+            Console.WriteLine( "the ID of employee "+e.Id);
+            Console.WriteLine("the name of employee " + e.Name);
+           
+            Console.WriteLine("the age of employee ");
+            //manger.Calc();
+            e.Calc();
+            e.Calc("7 Feb 1999");
+            e.print();
+            e.printt();
+           // manger.printt();
         }
     }
 }
